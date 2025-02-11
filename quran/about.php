850dd89be46e37ecd8ec3c05ca3d0388770b4a21
fatimazahraAITRAHOU <121510114+fatimazahraAITRAHOU@users.noreply.github.com>
2025-02-11 12:27:17 +0100

    Add files via upload

---
+++ quran/about.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- 
+    - primary meta tag
+  -->
+  <title>بلغوا عني ولو آية</title>
+  <meta name="title" content="EduWeb - The Best Program to Enroll for Exchange">
+  <meta name="description" content="This is an education html template made by codewithsadee">
+
+  <!-- 
+    - favicon
+  -->
+  <link rel="icon" href="./assets/images/favicon-32x32.png" type="image/x-icon">
+
+
+
+  <!-- 
+  
+    - custom css link
+  -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="./assets/css/style.css">
+
+  <!-- 
+    - google font link
+  -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;600;700;800&family=Poppins:wght@400;500&display=swap"
+    rel="stylesheet">
+
+  <!-- 
+    - preload images
+  -->
+  <link rel="preload" as="image" href="./assets/images/hero-bg.svg">
+  <link rel="preload" as="image" href="./assets/images/hero-banner-1.jpg">
+  <link rel="preload" as="image" href="./assets/images/hero-banner-2.jpg">
+  <link rel="preload" as="image" href="./assets/images/hero-shape-1.svg">
+  <link rel="preload" as="image" href="./assets/images/hero-shape-2.png">
+
+</head>
+
+<body id="top">
+
+  <!-- 
+    - #HEADER
+  -->
+
+  <?php require_once ("Header.php")?>
+  <main>
+    <article>
+      <section class="section about" id="about" aria-label="about">
+        <div class="container contAbout">
+
+          <figure class="about-banner">
+
+            <div class="img-holder" style="--width: 520; --height: 370;">
+              <img src="./assets/images/about-banner.jpg" width="520" height="370" loading="lazy" alt="about banner"
+                class="img-cover">
+            </div>
+
+            <img src="./assets/images/about-shape-3.png" width="722" height="540" loading="lazy" alt=""
+              class="shape about-shape-3">
+
+          </figure>
+
+          <div class="about-content">
+
+            <h2 class="h2 section-title">
+           <span class="span">من نحن؟ :</span>  
+            </h2>
+
+            <p style="font-size:20px;" class="section-text">
+              مرحبًا بك في موقع بلغوا عني ولو بآية، وجهتكِ المثلى لحفظ القرآن الكريم برواية ورش عن نافع، والمخصص للنساء الراغبات في الارتقاء بحفظ كتاب الله والعمل بتعاليمه.فنحن نؤمن بأن حفظ القرآن الكريم هو أساس الإيمان والارتقاء في الدرجات. لدى نهدف إلى توفير بيئة تعليمية آمنة ومشجعة للنساء، لتحقيق رؤية القرآن الكريم في حياتهن.و تساعدهن على تعلمه و حفظه بالطريقة الصحيحة وفق قواعد التجويد. 
+            </p>
+          </div>
+
+        </div>
+      </section>
+      <section class="section about" id="about" aria-label="about">
+        <div class="container contAbout">
+
+  
+
+          <div class="about-content">
+
+            <h2 class="h2 section-title">
+           <span class="span">رسالتنا :
+          </span>  
+            </h2>
+
+            <p style="font-size:20px;" class="section-text">
+              تمكين النساء من التعلق بكتاب الله، وتحقيق الحفظ المتقن والتدبر العميق لآياته، مستعينين بمنهج تعليمي متقن وأدوات مساعدة تناسب كل المستويات. و نركز على التوجيه الشخصي والتعليم المتبع، لتحقيق النجاح الروحي والاكاديمي.
+            </p>
+          </div>
+          <figure class="about-banner">
+
+            <div class="img-holder" style="--width: 320; --height: 470;">
+              <img src="./assets/images/image1.jpg" width="320" height="470" loading="lazy" alt="about banner"
+                class="img-cover">
+            </div>
+
+            <img src="./assets/images/about-shape-3.png" width="622" height="540" loading="lazy" alt=""
+              class="shape about-shape-3">
+
+          </figure>
+        </div>
+      </section>
+      
+      <section class="section about" id="about" aria-label="about">
+        <div class="container contAbout">
+
+          <figure class="about-banner">
+
+            <div class="img-holder" style="--width: 520; --height: 370;">
+              <img src="./assets/images/image3.jpg" width="520" height="370" loading="lazy" alt="about banner"
+                class="img-cover">
+            </div>
+
+            <img src="./assets/images/about-shape-3.png" width="722" height="540" loading="lazy" alt=""
+              class="shape about-shape-3">
+
+          </figure>
+
+          <div class="about-content">
+
+            <h2 class="h2 section-title">
+           <span class="span">رؤيتنا :</span>  
+            </h2>
+
+            <p style="font-size:20px;" class="section-text">
+              أن نكون منصة قرآنية رائدة تسهم في تكوين جيل من النساء الحافظات لكتاب الله، ليكنَّ منارات للخير في مجتمعاتهن. و تعليم القرآن بمفهومه الواسع والذي يشمل تعليم أحكام التلاوة والترتيل والتحفيظ والتفسير والتدبر وصولًا إلى العمل ؛ وذلك بأفضل الوسائل التربوية و أحسن الطرق التعليمية.
+
+نسعد بانضمامكِ إلينا في رحلتكِ القرآنية، ونسأل الله أن يجعلنا وإياكِ من أهل القرآن وخاصته.
+            </p>
+          </div>
+
+        </div>
+      </section>
+
+      
+
+    </article>
+  </main>
+
+
+
+
+
+  <!-- 
+    - #FOOTER
+  -->
+
+  <?php
+require_once('footer.php');
+?>
+
+
+
+
+
+  <!-- 
+    - #BACK TO TOP
+  -->
+
+  <a href="#top" class="back-top-btn" aria-label="back top top" data-back-top-btn>
+    <ion-icon name="chevron-up" aria-hidden="true"></ion-icon>
+  </a>
+
+
+
+
+
+  <!-- 
+    - custom js link
+  -->
+  <script src="./assets/js/script.js" defer></script>
+
+  <!-- 
+    - ionicon link
+  -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+
+</html>
